4f7bceab768b1ad019e3fa9316abd76e69a13bf7
Peerin <1461200635@qq.com>
2024-09-01 15:43:17 +0800

    init: complete basic blog structure

---
+++ scripts/main.js
@@ -0,0 +1,51 @@
+import * as markedjs from "./3rdparty/marked.esm.js";
+
+async function fetchBlogs() {
+    const blogDir = "../blogs";
+    const supportedFormats = [".md"]; //cannot support .org now
+    
+    let blogs = [];
+
+    let i = 1;
+    while(true){
+	let has = false;
+	for(const suffix of supportedFormats){
+	    const url = blogDir + "/" + i.toString() + suffix;
+	    const response = await fetch(url);
+	    if(response.ok){
+		blogs.push({
+		    'url': url,
+		    'content': await response.text()
+		});
+		has = true;
+	    }
+	}
+	if(!has){
+	    break;
+	}
+	i += 1;
+    }
+
+    const blogListElem = document.getElementById("blog-list");
+    for(const blog of blogs){
+	const liElem = document.createElement("li");
+	liElem.addEventListener("click", function(e){
+	    switchToBlog(blog.content);
+	});
+	liElem.append(blog.content.substring(0, 10));
+	blogListElem.append(liElem);
+    }
+
+    return true;
+}
+
+function switchToBlog(blogContent){
+    document.body.innerHTML = markedjs.marked.parse(blogContent);
+}
+
+fetchBlogs().then(ret => {
+    if(ret){
+	console.log("fetched all blogs");
+    }
+});
+
